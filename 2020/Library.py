b5ae26d0b8c6a023d5115a9695003af1b342cd54
thibautvangoethem <thibaut.vangoethem@student.uantwerpen.be>
2020-02-20 19:22:46 +0100

    baaaaaaa

--- 2020/Library.py
+++ 2020/Library.py
@@ -8,8 +8,11 @@ class Library:
         self.signup_time = signup_time
         self.books = list()
 
-    def add_book(self, book: Book):
+    def add_book(self, book):
         self.books.append(book)
+
+    def add_books(self, books):
+        self.books=(books)
 
     def get_scan_limit(self):
         return self.scan_limit
@@ -19,3 +22,6 @@ class Library:
 
     def get_books(self):
         return self.books
+
+    def __str__(self):
+        return str(self.book_count)+" "+str(+self.signup_time)+" "+str(self.scan_limit)+" "+str(self.books)
--- 2020/app.py
+++ 2020/app.py
@@ -17,15 +17,57 @@ print(r"""
  ----------`"`  `"``--------\|/----\|/-----
 
 """)
-filename="aaaaaaa"
+filename="a_example"
+from Library import *
 
+def orderLibraries(daysamount,pointsamount,libraries):
+    signupVolgorde=list()
+    for i in libraries:
+        signupVolgorde.append(i)
+    return signupVolgorde
 
 def algorithms(input):
-    gerief=input.read()
-    return
+    firsline=input.readline()
+    splitted=firsline.split()
+
+    booksAmount=int(splitted[0])
+    libraryAmount=int(splitted[1])
+    daysAmount=int(splitted[2])
+    print("%s %s %s" %(booksAmount,libraryAmount,daysAmount))
+
+    pointslist=list()
+    for i in input.readline().split():
+        pointslist.append(int(i))
+    print(pointslist)
+
+    libraries=list()
+    line=f.readline()
+    while line!="":
+
+        split=line.split()
+        templist = list()
+        a=int(split[0])
+        b=int(split[1])
+        c=int(split[2])
+        library = Library(a,b,c)
+        line=f.readline()
+        templist=list()
+        for i in line.split():
+            templist.append(int(i))
+        library.add_books(templist)
+        libraries.append(library)
+        line=f.readline()
+    for i in libraries:
+        print(i)
+
+    signupVolgorde=orderLibraries(daysAmount,pointslist,libraries)
+
+
+
+
 
 if  __name__ == "__main__":
     f=open("./input/"+filename+".txt")
     algorithms(f)
-    print(f)
+
 
